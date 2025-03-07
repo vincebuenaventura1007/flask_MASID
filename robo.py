2d3b962c35607846c99856b3ab9d04a93bad4208
vincebuenaventura1007 <123221622+vincebuenaventura1007@users.noreply.github.com>
2025-03-07 08:21:29 +0800

    Add files via upload

--- robo.py
+++ robo.py
@@ -116,4 +116,4 @@ def detect_image():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5001)))
